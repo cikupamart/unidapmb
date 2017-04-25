6397ebc7acc20a1e6fb558306afcb30dba05e2a7
virgantara <vinux.edu@gmail.com>
2017-04-25 13:47:09 +0700

    delete pmb

--- protected/controllers/PmbController.php
+++ protected/controllers/PmbController.php
@@ -197,14 +197,14 @@ class PmbController extends Controller
 	 * If deletion is successful, the browser will be redirected to the 'admin' page.
 	 * @param integer $id the ID of the model to be deleted
 	 */
-	// public function actionDelete($id)
-	// {
-	// 	$this->loadModel($id)->delete();
+	public function actionDelete($id)
+	{
+		$this->loadModel($id)->delete();
 
-	// 	// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-	// 	if(!isset($_GET['ajax']))
-	// 		$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
-	// }
+		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+		if(!isset($_GET['ajax']))
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+	}
 
 	// /**
 	//  * Lists all models.
