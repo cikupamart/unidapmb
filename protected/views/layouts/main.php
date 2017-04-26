e6a6f9dd1583713c6ed4aac5084e3b134fc96741
virgantara <vinux.edu@gmail.com>
2017-04-26 07:08:56 +0700

    add menu pendaftar

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,8 +32,9 @@
 		if(!Yii::app()->user->isGuest){
 		 $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
+				array('label'=>'Pendaftar', 'url'=>array('/pmb/admin')),
 				array('label'=>'Konten', 'url'=>array('/info/index')),
-				// array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				
 				// array('label'=>'Contact', 'url'=>array('/site/contact')),
 				// array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
